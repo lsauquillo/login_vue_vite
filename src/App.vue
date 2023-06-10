56921e568f5ad99324b696f7c99aa57198c384c6
lsauquillo <lsauquillo@gmail.com>
2023-06-10 07:39:46 +0200

    basico

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <nav>
+          <RouterLink  to="/">Home</RouterLink> |
+          <RouterLink  to="/login" >Login</RouterLink> |
+          <RouterLink  to="/register">Register</RouterLink> |
+          <button>Logout</button>
+  </nav>
+  <RouterView />
+ 
+</template>
+
+<style scoped>
+
+</style>
