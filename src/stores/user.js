af7dd861d4357347c1e10699966db70f6c2f1a65
lsauquillo <lsauquillo@gmail.com>
2023-06-16 10:11:41 +0200

    register_usuario

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,10 +1,26 @@
 import { defineStore} from 'pinia'
+import { createUserWithEmailAndPassword }from 'firebase/auth';
+import { auth } from '../firebaseConfig.js';
 
-export const useUserStore = defineStore('useStore', 
+export const useUserStore = defineStore('user', 
  {
   state: ()=>({
     userData: null
   }),
-  actions: {}
+  actions: {
+    async registerUser(email, password){
+      try{
+        const {user} = await createUserWithEmailAndPassword(auth, email, password)
+        //console.log(user)
+        this.userData = { email: user.email, uid: user.uid} 
+        console.log(this.userData.email)       
+      }
+      catch(err){
+        console.log(err)
+      }
+
+     
+    }
+  }
  }
 )
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,10 +1,36 @@
 <template>
    <h1>Register</h1>
+   <form @submit.prevent="handleSubmit">
+      <input type="email" placeholder="Intro email" v-model.trim="email">
+      <input type="password" placeholder="contraseña" v-model.trim="password">
+      <button type="submit">Registrarse</button>
+   </form>
+   <p>Usuario:{{ userStore.userData}}</p>
 </template>
 
 <script setup>
+
+import { useUserStore } from '../stores/user.js';
+import { useRouter } from 'vue-router'
+import { ref } from "vue";
+
+const userStore = useUserStore();
+const router = useRouter();
+
+   const email = ref('sauquillo@gmail.com')
+   const password = ref('12345678')
    
+   const handleSubmit = ()=>{
+      if( !email.value || password.value.length < 6 ){         
+         return alert('Debe rellenar los campos!')    
+      }
+      userStore.registerUser(email.value, password.value)
+      //console.log('Procesando formulario...') 
+      //console.log('Usuario registrado:', userStore.userData)
+      router.push('/') 
+   }                            
 </script>
+
 
 <style scoped>
   
